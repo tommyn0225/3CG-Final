895da837f702c06a0a050b88a32eadf358acab89
tommyn0225 <tommyn0225@gmail.com>
2025-06-12 22:01:14 -0700

    chore: added .love file

---
+++ conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+    t.title = "Mythic Clash"
+    t.version = "11.4"
+    t.window.width = 1680
+    t.window.height = 1050
+    t.window.resizable = false
+    t.window.fullscreen = false
+    
+    -- For debugging
+    t.console = true
+end 
